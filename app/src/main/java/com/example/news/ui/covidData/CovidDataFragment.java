20f409171a31d885d3b24af22f3b314935f1e2f0
Rachel Mao <rachelrmao99@hotmail.com>
2020-09-11 04:23:11 -0700

    formatting

--- app/src/main/java/com/example/news/ui/covidData/CovidDataFragment.java
+++ app/src/main/java/com/example/news/ui/covidData/CovidDataFragment.java
@@ -47,8 +47,11 @@ public class CovidDataFragment extends Fragment {
     HorizontalBarChart chart2;
     ProgressBar pg;
     HashMap<String, float[]> map = new HashMap<String, float[]>();
+    HashMap<String, float[]> map2 = new HashMap<String, float[]>();
     ArrayList<BarEntry> dataVals;
+    ArrayList<BarEntry> dataVals2;
     ArrayList<String> keyList;
+    ArrayList<String> keyList2;
     int[] colorClassArray = new int[]{Color.RED, Color.GREEN, Color.YELLOW};
     Button button1;
     Button button2;
@@ -62,7 +65,7 @@ public class CovidDataFragment extends Fragment {
         chart.setNoDataText("");
 
         chart2 = (HorizontalBarChart) root.findViewById(R.id.china);
-        chart2.setNoDataText("china");
+        chart2.setNoDataText("");
 
         pg = (ProgressBar) root.findViewById(R.id.progressbar);
 
@@ -85,6 +88,8 @@ public class CovidDataFragment extends Fragment {
         });
 
         dataVals = new ArrayList<>();
+        dataVals2 = new ArrayList<>();
+
         new Thread(new Runnable() {
             public void run() {
                 URL url = null;
@@ -116,20 +121,42 @@ public class CovidDataFragment extends Fragment {
                             fdata[2] = (float) cases.getDouble(0) - fdata[1];
                             map.put(key, fdata);
                         }
+                        else if (evaluate(key, "china")) {
+//                            Log.d("debug", key);
+                            JSONObject obj = jo.getJSONObject(key);
+                            JSONArray data = obj.getJSONArray("data");
+                            JSONArray cases =(JSONArray) data.get(data.length() - 1);
+                            float [] fdata = new float [3];
+                            fdata[0] = (float) cases.getDouble(3);
+                            fdata[1] = (float) cases.getDouble(2) - fdata[0];
+                            fdata[2] = (float) cases.getDouble(0) - fdata[1];
+                            map2.put(key.substring(6, key.length()), fdata);
+//                            Log.d("debug", key.substring(6, key.length()));
+//                            Log.d("debug", fdata[0] + " " + fdata[1] + " " + fdata[2]);
+                        }
                     }
                     keyList = new ArrayList(map.keySet());
+                    keyList2 = new ArrayList(map2.keySet());
+
                     float count = 0;
                     for (Map.Entry<String,float[]> entry : map.entrySet()) {
-//                        Log.d("debug", String.valueOf(entry.getKey()));
-//                        for (int i = 0; i < 3; i++) Log.d("debug", String.valueOf(entry.getValue()[i]));
                         dataVals.add(new BarEntry(count, entry.getValue()));
                         count = count + 1;
                     }
+                    count = 0;
+                    for (Map.Entry<String,float[]> entry : map2.entrySet()) {
+                        dataVals2.add(new BarEntry(count, entry.getValue()));
+                        count = count + 1;
+                    }
+
                     Handler threadHandler = new Handler(Looper.getMainLooper());
                     threadHandler.post(new Runnable() {
                         @Override
                         public void run() {
                             pg.setVisibility(View.GONE);
+
+                            drawGraph(chart, dataVals, keyList);
+                            drawGraph(chart2, dataVals2, keyList2);
 //                            for (int i = 0; i < dataVals.size(); i++) {
 //                                BarEntry entry = dataVals.get(i);
 //                                Log.d("debug", String.valueOf(entry.getX()));
@@ -139,76 +166,74 @@ public class CovidDataFragment extends Fragment {
 //                            }
 
                             // Styling
-//                            chart = (HorizontalBarChart) root.findViewById(R.id.stacked_BarChart);
-//                            chart.setNoDataText("");
 
-                            XAxis xAxis = chart.getXAxis();
-                            xAxis.setValueFormatter(new IndexAxisValueFormatter(keyList));
-
-                            xAxis.setDrawLabels(true);
-                            xAxis.setDrawAxisLine(true);
-                            xAxis.setDrawGridLines(false);
-                            xAxis.setTextColor(getResources().getColor(R.color.white));
-                            xAxis.setAxisLineColor(getResources().getColor(android.R.color.white));
-                            xAxis.setAxisLineWidth(2);  // y ashix line size
-                            xAxis.setGranularityEnabled(true);
-                            xAxis.setCenterAxisLabels(false);
-                            xAxis.setGranularity(1f);
-                            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
-                            YAxis yAxis = chart.getAxisLeft();
-                            yAxis.setDrawAxisLine(true);
-                            yAxis.setDrawGridLines(false);
-                            yAxis.setTextColor(getResources().getColor(R.color.white));
-                            yAxis.setAxisLineColor(getResources().getColor(R.color.white));
-                            yAxis.setAxisLineWidth(2);  // y ashix line size
-                            yAxis.setAxisMinValue(0);
-                            yAxis.setLabelCount(5);
-
-                            YAxis rightAxis = chart.getAxisRight();
-                            rightAxis.setDrawLabels(false);
-
-                            chart.setBackgroundColor(getResources().getColor(R.color.pHubSideBar));
-                            chart.setGridBackgroundColor(getResources().getColor(R.color.pHubSideBar));
-
-                            BarDataSet barDataSet = null;
-                            barDataSet = new BarDataSet(dataVals,"");
-                            barDataSet.setColors(colorClassArray);
-                            barDataSet.setDrawValues(false);
-
-                            BarData barData = new BarData(barDataSet);
-                            barData.setBarWidth(1f);
-
-                            chart.setData(barData);
-                            chart.getDescription().setText("");
-                            chart.setVerticalScrollBarEnabled(true);
-                            chart.setHorizontalScrollBarEnabled(true);
-                            chart.getLegend().setEnabled(true);
-                            chart.getLegend().setTextColor(getResources().getColor(R.color.white));
-                            Legend legend = chart.getLegend();
-
-                            String [] titleList = new String[] {"Deaths", "Cured", "Confirmed"};
-                            List<LegendEntry> entries = new ArrayList<>();
-                            for (int i = 0; i < 3; i++) {
-                                LegendEntry entry = new LegendEntry();
-                                entry.formColor = colorClassArray[i];
-                                entry.label = titleList[i];
-                                entries.add(entry);
-                            }
-                            legend.setCustom(entries);
-
-
-                            chart.setDrawGridBackground(false);
-                            chart.setDoubleTapToZoomEnabled(false);
-                            chart.setVisibleXRangeMaximum(25);
-                            chart.moveViewToX(1);
-
-                            chart.notifyDataSetChanged();
-                            chart.invalidate();
-
-                            xAxis.setLabelCount(202, true);
-                            xAxis.setTextSize(10f);
-                            Log.d("debug", "label count: " + String.valueOf(xAxis.getLabelCount()));
+//                            XAxis xAxis = chart.getXAxis();
+//                            xAxis.setValueFormatter(new IndexAxisValueFormatter(keyList));
+//
+//                            xAxis.setDrawLabels(true);
+//                            xAxis.setDrawAxisLine(true);
+//                            xAxis.setDrawGridLines(false);
+//                            xAxis.setTextColor(getResources().getColor(R.color.white));
+//                            xAxis.setAxisLineColor(getResources().getColor(android.R.color.white));
+//                            xAxis.setAxisLineWidth(2);  // y ashix line size
+//                            xAxis.setGranularityEnabled(true);
+//                            xAxis.setCenterAxisLabels(false);
+//                            xAxis.setGranularity(1f);
+//                            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//
+//                            YAxis yAxis = chart.getAxisLeft();
+//                            yAxis.setDrawAxisLine(true);
+//                            yAxis.setDrawGridLines(false);
+//                            yAxis.setTextColor(getResources().getColor(R.color.white));
+//                            yAxis.setAxisLineColor(getResources().getColor(R.color.white));
+//                            yAxis.setAxisLineWidth(2);  // y ashix line size
+//                            yAxis.setAxisMinValue(0);
+//                            yAxis.setLabelCount(5);
+//
+//                            YAxis rightAxis = chart.getAxisRight();
+//                            rightAxis.setDrawLabels(false);
+//
+//                            chart.setBackgroundColor(getResources().getColor(R.color.pHubSideBar));
+//                            chart.setGridBackgroundColor(getResources().getColor(R.color.pHubSideBar));
+//
+//                            BarDataSet barDataSet = null;
+//                            barDataSet = new BarDataSet(dataVals,"");
+//                            barDataSet.setColors(colorClassArray);
+//                            barDataSet.setDrawValues(false);
+//
+//                            BarData barData = new BarData(barDataSet);
+//                            barData.setBarWidth(1f);
+//
+//                            chart.setData(barData);
+//                            chart.getDescription().setText("");
+//                            chart.setVerticalScrollBarEnabled(true);
+//                            chart.setHorizontalScrollBarEnabled(true);
+//                            chart.getLegend().setEnabled(true);
+//                            chart.getLegend().setTextColor(getResources().getColor(R.color.white));
+//                            Legend legend = chart.getLegend();
+//
+//                            String [] titleList = new String[] {"Deaths", "Cured", "Confirmed"};
+//                            List<LegendEntry> entries = new ArrayList<>();
+//                            for (int i = 0; i < 3; i++) {
+//                                LegendEntry entry = new LegendEntry();
+//                                entry.formColor = colorClassArray[i];
+//                                entry.label = titleList[i];
+//                                entries.add(entry);
+//                            }
+//                            legend.setCustom(entries);
+//
+//
+//                            chart.setDrawGridBackground(false);
+//                            chart.setDoubleTapToZoomEnabled(false);
+//                            chart.setVisibleXRangeMaximum(25);
+//                            chart.moveViewToX(1);
+//
+//                            chart.notifyDataSetChanged();
+//                            chart.invalidate();
+//
+//                            xAxis.setLabelCount(202, true);
+//                            xAxis.setTextSize(10f);
+//                            Log.d("debug", "label count: " + String.valueOf(xAxis.getLabelCount()));
                         }
                     });
 
@@ -227,6 +252,78 @@ public class CovidDataFragment extends Fragment {
 
         return root;
     }
+
+
+    private void drawGraph(HorizontalBarChart chart, ArrayList<BarEntry> dataVals, ArrayList<String> keys) {
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(keys));
+
+        xAxis.setDrawLabels(true);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setTextColor(getResources().getColor(R.color.white));
+        xAxis.setAxisLineColor(getResources().getColor(android.R.color.white));
+        xAxis.setAxisLineWidth(2);  // y ashix line size
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(false);
+        xAxis.setGranularity(1f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setDrawAxisLine(true);
+        yAxis.setDrawGridLines(false);
+        yAxis.setTextColor(getResources().getColor(R.color.white));
+        yAxis.setAxisLineColor(getResources().getColor(R.color.white));
+        yAxis.setAxisLineWidth(2);  // y ashix line size
+        yAxis.setAxisMinValue(0);
+        yAxis.setLabelCount(5);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawLabels(false);
+
+        chart.setBackgroundColor(getResources().getColor(R.color.pHubSideBar));
+        chart.setGridBackgroundColor(getResources().getColor(R.color.pHubSideBar));
+
+        BarDataSet barDataSet = null;
+        barDataSet = new BarDataSet(dataVals,"");
+        barDataSet.setColors(colorClassArray);
+        barDataSet.setDrawValues(false);
+
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(1f);
+
+        chart.setData(barData);
+        chart.getDescription().setText("");
+        chart.setVerticalScrollBarEnabled(true);
+        chart.setHorizontalScrollBarEnabled(true);
+        chart.getLegend().setEnabled(true);
+        chart.getLegend().setTextColor(getResources().getColor(R.color.white));
+        Legend legend = chart.getLegend();
+
+        String [] titleList = new String[] {"Deaths", "Cured", "Confirmed"};
+        List<LegendEntry> entries = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            LegendEntry entry = new LegendEntry();
+            entry.formColor = colorClassArray[i];
+            entry.label = titleList[i];
+            entries.add(entry);
+        }
+        legend.setCustom(entries);
+
+
+        chart.setDrawGridBackground(false);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setVisibleXRangeMaximum(25);
+        chart.moveViewToX(1);
+
+        chart.notifyDataSetChanged();
+        chart.invalidate();
+
+        xAxis.setLabelCount(dataVals.size(), true);
+        xAxis.setTextSize(10f);
+        Log.d("debug", "label count: " + String.valueOf(xAxis.getLabelCount()));
+    }
+
 
     private boolean evaluate (String key, String select) {
         if (select == "china") {
